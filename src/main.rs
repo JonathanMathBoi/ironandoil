8d5e6de580b6f7fb1c0280f4d037cdb8e8fe0988
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-13 16:44:20 -0500

    Basic Web Server Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 // SPDX-FileCopyrightText: 2026 Jonathan Hart
 // SPDX-License-Identifier: AGPL-3.0-only
 
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello World!"
 }
